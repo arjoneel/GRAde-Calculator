ae97c2cc62698da81c84503c4c48fb2fedc11479
arjoneel <112979746+arjoneel@users.noreply.github.com>
2022-09-06 16:41:40 -0400

    1.0

---
+++ ism4300/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace ism4300
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MarksEarnedTextBox = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(283, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 51);
+            this.label1.TabIndex = 1;
+            this.label1.Text = " Grade Calculator ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(372, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 51);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "ISM 4300";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(75, 319);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(310, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter Marks Earned:";
+            // 
+            // MarksEarnedTextBox
+            // 
+            this.MarksEarnedTextBox.Location = new System.Drawing.Point(391, 325);
+            this.MarksEarnedTextBox.Name = "MarksEarnedTextBox";
+            this.MarksEarnedTextBox.Size = new System.Drawing.Size(194, 31);
+            this.MarksEarnedTextBox.TabIndex = 4;
+            this.MarksEarnedTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(239, 569);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(196, 49);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Calculate ";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(456, 569);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(196, 49);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(75, 439);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(316, 37);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Your Letter Grade Is:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(407, 448);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 25);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 751);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.MarksEarnedTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox MarksEarnedTextBox;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ ism4300/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ism4300
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            int marks = 0;
+
+            marks = Convert.ToInt32(MarksEarnedTextBox.Text);
+
+            //ifelse 
+            //98 - 100
+            string grade = "-";
+            if (marks >= 98 && marks <= 100)
+            {
+                grade = "A+";
+            }
+
+            else if (marks >= 92 && marks <= 97)
+            {
+                grade = "A";
+
+            }
+            else if (marks >= 90 && marks <= 91)
+            {
+                grade = "A-";
+            }
+
+            else if (marks >= 88 && marks <= 89)
+            {
+                grade = "B+";
+            }
+
+            else if (marks >= 82 && marks <= 87)
+            {
+                grade = "B";
+            }
+
+            else if (marks >= 80 && marks <= 81)
+            {
+                grade = "B-";
+            }
+
+            else if (marks >= 78 && marks <= 79)
+            {
+                grade = "C+";
+            }
+
+            else if (marks >= 72 && marks <= 77)
+            {
+                grade = "C";
+            }
+
+            else if (marks >= 70 && marks <= 71)
+            {
+                grade = "C-";
+            }
+
+            else if (marks >= 68 && marks <= 69)
+            {
+                grade = "D+";
+            }
+
+            else if (marks >= 62 && marks <= 67)
+            {
+                grade = "D";
+            }
+
+            else if (marks >= 60 && marks <= 61)
+            {
+                grade = "D-";
+            }
+
+            else if (marks >= 0 && marks <= 59)
+            {
+                grade = "E";
+            }
+
+            else if (marks <= 0 || marks >= 100)
+            {
+               grade = "Enter valid Grade.";
+            }
+
+            label5.Text = grade.ToString();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            MarksEarnedTextBox.Text = string.Empty;
+            label5.Text = "-";
+        }
+    }
+}
